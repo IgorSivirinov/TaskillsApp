1b6088b2b4756d7034cd3ca999b6b5d4f92fe0ef
titrom <71824652+titrom@users.noreply.github.com>
2021-11-18 17:01:12 +0300

    update

---
+++ Taskills.WebAppMVC/Controllers/ListController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Taskills.WebAppMVC.Controllers;
+public class ListController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Taskills.WebAppMVC/wwwroot/js/cam.js
@@ -0,0 +1,43 @@
+﻿
+function check() {
+    if (document.cameraForm.cam[0].checked) {
+        document.getElementById("downloadCam").innerHTML =
+            `<input type="file" accept="image/jpeg">`
+        document.getElementById("camera").innerHTML =
+            `Сделать фото`
+    }
+    if (document.cameraForm.cam[1].checked) {
+        document.getElementById("downloadCam").innerHTML =
+            `Загрузить Фото`
+        document.getElementById("camera").innerHTML =
+            `<div id="myCam" >sfas</div>
+                <button type="button" onclick="Cam()">Сделать</button>
+               <button type="button" onclick="startCam()">Заново</button>`
+        startCam();
+    }
+}
+
+
+
+function startCam() {
+    Webcam.set({
+        wight: 450,
+        height: 450,
+        image_format: "jpeg",
+        jpeg_quality: 90
+    });
+    Webcam.attach('#myCam');
+}
+
+function Cam() {
+    take_snapshot();
+}
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById('myCam').innerHTML =
+            `<img src="${data_uri}">`
+    });
+    Webcam.off();
+
+}
